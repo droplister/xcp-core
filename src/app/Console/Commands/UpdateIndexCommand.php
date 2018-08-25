3e68b44fb9ee4bbe329085c3e2fd9da182a6112d
Dan Anderson <anderson@familymedia.org>
2018-08-25 01:35:09 -0400

    Remove Switch (add to apps)

--- src/app/Console/Commands/UpdateIndexCommand.php
+++ src/app/Console/Commands/UpdateIndexCommand.php
@@ -65,9 +65,6 @@ class UpdateIndexCommand extends Command
             // Only fire when necessary
             if($this->isNewBlockIndex())
             {
-                // Useful Switch
-                if(config('xcp-core.syncing')) return false;
-
                 // Update blocks
                 $this->call('update:blocks');
 
